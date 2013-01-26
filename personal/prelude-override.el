b969abf7588bac8d456519e842c80351e3446db7
Julien Wintz <julien.wintz@me.com>
2013-01-26 22:51:42 +0100

    Overriding ido mode.

--- personal/prelude-override.el
+++ personal/prelude-override.el
@@ -1,7 +1,20 @@
+# Helper functions
+
 (defun disable-guru-mode ()
   (guru-mode -1))
 
+# Disable prelude whitespaces
+
 (setq prelude-whitespace nil)
+
+# Disable preludes flyspell
+
 (setq prelude-flyspell nil)
 
+# Disable prelude guru mode
+
 (add-hook 'prelude-prog-mode-hook 'disable-guru-mode t)
+
+# Disable ido-mode
+
+(ido-mode 0)
